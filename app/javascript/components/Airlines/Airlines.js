7915c8294300860716b66f9b66e973135bd20f38
Permz <sakanaquarium0mashim0@gmail.com>
2021-01-10 23:48:00 +0900

    ReviewForm component 作成

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment as div } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Airline from './Airline'
 import styled from 'styled-components';
